90b8efd84a50f0486e2da8924795f61335f46600
姚鑫 <charlesyaoxin@163.com>
2017-01-09 17:52:39 +0800

    Initial Commit

---
+++ GCD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCD
+//
+//  Created by Charles on 17/1/9.
+//  Copyright © 2017年 Charles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GCD/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GCD
+//
+//  Created by Charles on 17/1/9.
+//  Copyright © 2017年 Charles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
